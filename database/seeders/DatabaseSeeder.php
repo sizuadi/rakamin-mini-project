7ae24bcb7f67ee09f92687064602f0991b72a28f
sizuwanoadi <46958807+sizuwanoadi@users.noreply.github.com>
2021-11-25 09:37:51 +0700

    add : Seeder and README

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
+use Eloquent;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Eloquent::unguard(); 
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        
+        $this->call(UsersTableSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
